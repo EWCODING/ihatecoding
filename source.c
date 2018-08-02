e53d4727209a796fc8effbcbff31ca7d264c58e9
Unknown <mariastokes@live.co.uk>
2018-08-02 20:32:24 +0100

    Read in file
    - created file pointer
    - started planning how to read the file in

---
+++ source.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void main (int argc, char* argv[])
+
+	{
+		FILE* FilePointer;
+		FilePointer = NULL;
+		
+		fopen_s(&FilePointer, argv[1],"r");
+		
+
+
+
+
+	
+	}
+
