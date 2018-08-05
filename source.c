492cbc45f1f11228fa471f7821e3f2a41fcb8d02
Unknown <mariastokes@live.co.uk>
2018-08-05 14:21:29 +0100

    Created Readinfo and ReadNumber function
    created struct for the intensity triplets

--- source.c
+++ source.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 
 
+int ReadNumber(FILE *File);
+char ReadInfo(FilePointer);
+
+
+typedef struct Pixel {		//a struct defining each pixel in the image
+	int R;
+	int G;
+	int B;
+}Pixel;
+
+
 
 void main (int argc, char* argv[])
 
@@ -9,32 +20,74 @@ void main (int argc, char* argv[])
 		FILE* FilePointer;
 		FilePointer = NULL;
 
+		int nRows;
+		int nColumns;
+		char DescriptionLine;
+
 		fopen_s(&FilePointer, argv[1],"r");
 
+		nRows = ReadNumber(FilePointer);
+		nColumns = ReadNumber(FilePointer);
+
+		DescriptionLine = ReadInfo(FilePointer);
 
 
+		Pixel **InputImage = (Pixel **)malloc(nRows * sizeof(Pixel *));			//defines 2d array
 
 
 
 	}
+//Function 3
 
 
-//maria, this whole function should be rewritten with different variable names and comments
 
-int read1Int(FILE *file) {
-/*this function accepts a file 'object' and returns an integer,
-from the space seperated file. it leverages the fscanf function which reads
-from a file until it reaches a 'space' character. this is very convenient */
 
-	char tempBuffer[15];          					 //will allow this to work with numbers up to 10^1024 (ish)
-	int number;
-	char* delimiter;
-	fscanf_s(file, "%15s", tempBuffer, 15);          //read from the file object, determines that it is a string type and stores in the buffer
-	number = strtol(tempBuffer, &delimiter, 10);	//returns an integer into number and a char pointer to delimiter
-	if (*delimiter != *tempBuffer){					//if a conversion didnt happen, the delimiter will be equal to the original value of tempbuffer. error condition.
-		return(number);								//if it happened, return it.
+	//Function 2
+char* ReadInfo (FILE *File)
+{
+	//function to read in the desctription information from the second line
+	//Characters read in from the line saved to variable DescriptionLine
+	//and then assigned to CurrentCharacter
+
+	char DescriptionLine[255];
+	char CurrentCharacter;
+	int i=0;
+
+	while((i<255) && (CurrentCharacter!='\n'))
+	{
+		scanf_s(file,"%1s",CurrentCharacter);
+		DescriptionLine[i] = CurrentCharacter;
+		i++;
 	}
-	else{
-		//figure out a nice way to exit the program
-	}
+		DescriptionLine[i] = \0;				//makes the last part of the string a terminator
+}
+
+
+	//Function 1
+	int ReadNumber(FILE *File)
+{
+		//Function reads in an integer from the input file as a string
+		//and then converts it to an integer. The fscanf_s function
+		//stops when a space is reached. Strtoll ignores anyhting
+		//that's not an integer.
+
+		char InputString[15];          													 //Creating a character array to store the data from the fil being read in
+		int Integer;
+		char* StopAddress;																			//stores the address of where the the fscanf stops to variable StopAddress
+
+		fscanf_s(file, "%15s", InputString, 15);          			//Reads in the information from the file and stores it to the variable InputString
+		Integer = strtol(InputString, &StopAddress, 10);				//Converts the string InputString to an integer value stored as Integer
+
+		                                                        //If statement to check whether or not the string has been converted
+																														//Works by comparing the start address to the end address of the InputString
+																														//If they are different a conversion has been made otherwise the conversion
+																														//has failed and there has been an error
+		if (*StopAddress != *InputString)
+		{
+			return(Integer);
+		}																												//If a string was converted to an integer, the integer is returned
+		else
+		{
+			printf("The input file is not valid!"\n);	           //Error message to let user know the file is not valid
+		}
 }
